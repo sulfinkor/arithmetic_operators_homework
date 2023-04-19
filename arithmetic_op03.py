b63ec81b20cdfb6336ea6af79a79773ae00a9aa7
sulfinkor <sulfinkorsafarov@gmail.com>
2023-04-19 22:09:51 +0500

    killer

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,8 @@
-#Create a variable called 'number' and assign it the two-digit number
-
+#reate a variable called 'number' and assign it the two-digit number#C
+number=23
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer=32
 #Print the value of the 'answer'
+print(answer)
 
 
